0e9e878b2b9f700c7f3d74a74d6eb6c167e23e63
Leonardo Fabian Romero Perez <leoromero@MacBook-Pro-de-Leonardo.local>
2022-01-31 18:51:24 -0500

    initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,25 @@
-import { createSignal, createEffect, onCleanup } from 'solid-js';
-import './index.css';
+import logo from './logo.svg';
+import styles from './App.module.css';
 
 function App() {
-  const [counter, setCounter] = createSignal(0);
-  console.log('Rende cuerpo de la función');
-
-  // const counter = getCounter()
-
-  // createEffect(prev => {
-  //   const sum = prev + counter()
-  //   console.log(sum)
-  //   console.log(prev, counter())
-  //   return sum
-  // }, 0);
-
-
-  // const counter = getCounter()
-  // setCounter(counter + 1)
-
-  // const interval = setInterval(() => {
-  //   setCounter( counter() + 1)
-  // }, 1000)
-
-  // onCleanup(() => {
-  //   clearInterval(interval)
-  // })
-
   return (
-    <>
-      <h1>Nuestra contador</h1>
-      <p>{counter}</p>
-      {/* <p>{counter % 2 === 0 ? 'Es par' : 'Es impar'}</p> */}
-      <button onClick={() => setCounter(counter => counter + 1)}>Incrementar</button>
-    </>
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
